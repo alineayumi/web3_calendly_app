dbd63a2439495492e31c4a73afb225aeeef1cc0c
Aline Ayumi <aline.ayumi@unico.io>
2022-07-09 14:01:16 -0300

    feat: adding tailwind css boilerplate

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="bg-gray-900">
+      <div className="mx-auto h-screen max-w-screen-xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="font-semiBold text-base uppercase tracking-wide text-white">
+            Welcome to
+          </h2>
+          <p className="my-3 text-4xl font-bold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
+            Calend3
+          </p>
+          <p className="text-xl text-gray-100">Aline Ayumi.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
