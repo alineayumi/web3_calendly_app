a63e48d5bb2feca4cf94d6b468c6076e05372a77
Aline Ayumi <aline.ayumi@unico.io>
2022-07-22 22:25:57 -0300

    feat: adding routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,72 +1,13 @@
-import detectEthereumProvider from '@metamask/detect-provider'
-import Calendar from 'components/calendar'
-import { useEffect, useState } from 'react'
+import Layout from 'components/layout'
+import Home from './pages/home'
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
-  const [account, setAccount] = useState(null)
-
-  useEffect(() => {
-    checkAccountConnected()
-  }, [])
-
-  const checkAccountConnected = async () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const provider: any = await detectEthereumProvider()
-    const accounts = await provider.request({ method: 'eth_accounts' })
-
-    if (accounts.length > 0) {
-      setAccount(accounts[0])
-    } else {
-      console.log('Not authorized  account found')
-    }
-  }
-
-  const handleConnectWallet = async () => {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const provider: any = await detectEthereumProvider()
-
-      if (provider) {
-        // returns an array of accounts
-        const accounts = await provider.request({
-          method: 'eth_requestAccounts'
-        })
-
-        if (accounts.length > 0) {
-          setAccount(accounts[0])
-        } else {
-          alert('No account found')
-        }
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   return (
-    <div className="bg-gray-900">
-      <div className="mx-auto h-full max-w-screen-xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <h2 className="font-semiBold text-base uppercase tracking-wide text-white">
-            Welcome to
-          </h2>
-          <p className="my-3 text-4xl font-bold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
-            Calend3
-          </p>
-          <p className="mb-6 text-xl text-gray-100">
-            A web3 appointment scheduling.
-          </p>
-          {!account && (
-            <button
-              className="rounded bg-white p-2"
-              onClick={handleConnectWallet}
-            >
-              Connect Wallet
-            </button>
-          )}
-          {account && <Calendar account={account} />}
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />} />
+      </Route>
+    </Routes>
   )
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,16 @@ import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from './App'
 import { StyledEngineProvider } from '@mui/material'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
 root.render(
-  <StyledEngineProvider injectFirst>
-    <App />
-  </StyledEngineProvider>
+  <BrowserRouter>
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
+  </BrowserRouter>
 )
