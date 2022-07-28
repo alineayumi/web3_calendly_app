f3c9c5ed4bfbd5091cc2c510eeff592278b538a8
Aline Ayumi <aline.ayumi@unico.io>
2022-07-27 21:39:27 -0300

    feat: adding sign-in route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import Layout from 'components/layout'
 import Home from './pages/home'
 import { Routes, Route } from 'react-router-dom'
+import client from 'services/connectors'
+import { WagmiConfig } from 'wagmi'
+import SignIn from 'pages/sign_in'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="" element={<Home />} />
-      </Route>
-    </Routes>
+    <WagmiConfig client={client}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<Home />} />
+          <Route path="sign-in" element={<SignIn />} />
+        </Route>
+      </Routes>
+    </WagmiConfig>
   )
 }
